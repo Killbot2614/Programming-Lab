236d312245bdd5c12b0fe201d8eda0834e0c757d
Albert <45996655+Killbot2614@users.noreply.github.com>
2021-09-03 00:18:56 +0300

    Update Exp_04 - Fibonacci Series.py

--- Python/Exp_04 - Fibonacci Series.py
+++ Python/Exp_04 - Fibonacci Series.py
@@ -12,18 +12,19 @@
 # Step 10:Else
 # Step 11:Repeat from steps 4 to 7
 
-n=int(input(""))
-a=0
-b=1
-sum=0
-count=1
-print(end="")
-while(count<=n):
-    print(sum,end=" ")
-    count+=1
-    a=b
-    b=sum
-    sum=a+b
+n = int(input(""))
+a = 0
+b = 1
+sum = 0
+count = 1
+print(end = "")
+
+while(count <= n):
+    print(sum, end = " ")
+    count += 1
+    a = b
+    b = sum
+    sum = a + b
 
 
 # Input: 5
