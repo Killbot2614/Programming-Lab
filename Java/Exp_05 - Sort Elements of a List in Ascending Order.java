5ca7bf57d90fed9b0fe6295861b3c662475026c6
Albert <45996655+Killbot2614@users.noreply.github.com>
2021-09-02 23:44:55 +0300

    Update Exp_05 - Sort Elements of a List in Ascending Order.java

--- Java/Exp_05 - Sort Elements of a List in Ascending Order.java
+++ Java/Exp_05 - Sort Elements of a List in Ascending Order.java
@@ -1,3 +1,5 @@
+// Java program to sort the elements of list in ascending order.
+
 /** STEP 1: Declare and initialize an array.
 * STEP 2: Loop through the array and select an element.
 * STEP 3: The inner loop will be used to compare the selected element from the outer loop with the rest of the elements of the array.
@@ -6,7 +8,7 @@
 */
 
 import java.util.Scanner;
-public class JavaExample 
+public class Main 
 {
     public static void main(String[] args) 
     {
