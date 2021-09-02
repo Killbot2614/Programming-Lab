8b7db5beddb549f92b373197b5e7b3d10b29e13c
Albert <45996655+Killbot2614@users.noreply.github.com>
2021-09-03 00:10:42 +0300

    Update Exp_04 - Fibonacci Series.java

--- Java/Exp_04 - Fibonacci Series.java
+++ Java/Exp_04 - Fibonacci Series.java
@@ -1,16 +1,16 @@
 // Java Program to print the Fibonacci Series.
 
-/** Step 1:Input the 'n' value until which the Fibonacci series has to be generated
-* Step 2:Initialize sum = 0, a = 0, b = 1 and count = 1
-* Step 3:while (count <= n)
-* Step 4:print sum
-* Step 5:Increment the count variable
-* Step 6:swap a and b
-* Step 7:sum = a + b
-* Step 8:while (count > n)
-* Step 9:End the algorithm
-* Step 10:Else
-* Step 11:Repeat from steps 4 to 7
+/*  Step 1:Input the 'n' value until which the Fibonacci series has to be generated
+  Step 2:Initialize sum = 0, a = 0, b = 1 and count = 1
+  Step 3:while (count <= n)
+  Step 4:print sum
+  Step 5:Increment the count variable
+  Step 6:swap a and b
+  Step 7:sum = a + b
+  Step 8:while (count > n)
+  Step 9:End the algorithm
+  Step 10:Else
+  Step 11:Repeat from steps 4 to 7
 */
 
 
@@ -34,15 +34,15 @@ public class Fibonacci
 	}
 }
 
-/** (1)
-* Input: 5
-* Output: 0 1 1 2 3
+/*  (1)
+  Input: 5
+  Output: 0 1 1 2 3
 
-* (2)
-* Input: 8
-* Output: 0 1 1 2 3 5 8 13
+  (2)
+  Input: 8
+  Output: 0 1 1 2 3 5 8 13
 
-* (3)
-* Input: 12
-* Output: 0 1 1 2 3 5 8 13 21 34 55 89
+  (3)
+  Input: 12
+  Output: 0 1 1 2 3 5 8 13 21 34 55 89
 */
