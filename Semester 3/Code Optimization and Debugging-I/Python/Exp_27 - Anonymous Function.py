106993de1ff06e1bee0a80689b9800b291664570
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-09-06 18:10:46 +0300

    Update Exp_27 - Anonymous Function.py

--- Semester 3/Code Optimization and Debugging-I/Python/Exp_27 - Anonymous Function.py
+++ Semester 3/Code Optimization and Debugging-I/Python/Exp_27 - Anonymous Function.py
@@ -6,12 +6,12 @@ for c in range(a): print("2 raised to power", c, "is", b[c])
 
 # Input: The total terms are: 10
 # Output: 2 raised to power 0 is 1
-#		   2 raised to power 1 is 2
-#		   2 raised to power 2 is 4
-#		   2 raised to power 3 is 8
-#		   2 raised to power 4 is 16
-#		   2 raised to power 5 is 32
-#		   2 raised to power 6 is 64
-#		   2 raised to power 7 is 128
-#		   2 raised to power 8 is 256
-#		   2 raised to power 9 is 512
+#         2 raised to power 1 is 2
+#         2 raised to power 2 is 4
+#         2 raised to power 3 is 8
+#         2 raised to power 4 is 16
+#         2 raised to power 5 is 32
+#         2 raised to power 6 is 64
+#         2 raised to power 7 is 128
+#         2 raised to power 8 is 256
+#         2 raised to power 9 is 512
