2c58607eabf03a729be49012390dacd3a8244e83
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-09-08 09:03:44 +0300

    Update Exp_14 - Convert Binary Number to Decimal.cpp

--- Semester 3/Code Optimization and Debugging-I/C++/Exp_14 - Convert Binary Number to Decimal.cpp
+++ Semester 3/Code Optimization and Debugging-I/C++/Exp_14 - Convert Binary Number to Decimal.cpp
@@ -1,12 +1,12 @@
 // C++ Program to Convert Binary Number to Decimal and vice-versa.
-/*
-1. The user is asked to enter a binary number and it is stored in the variable ‘num’.
-2. The variable ‘b’ is initialized as 1 and ‘dec’ as 0. num is copied to the variable ‘temp’.
-3. Using a while loop, modulus of temp is stored in ‘rem’.
-4. rem is multiplied with b and added to dec.
-5. In every iteration, b is multiplied with 2.
-6. The loop continues till temp is less than 0.
-7. The decimal equivalent is stored in dec and printed.
+
+/* 1. The user is asked to enter a binary number and it is stored in the variable ‘num’.
+   2. The variable ‘b’ is initialized as 1 and ‘dec’ as 0. num is copied to the variable ‘temp’.
+   3. Using a while loop, modulus of temp is stored in ‘rem’.
+   4. rem is multiplied with b and added to dec.
+   5. In every iteration, b is multiplied with 2.
+   6. The loop continues till temp is less than 0.
+   7. The decimal equivalent is stored in dec and printed.
 */
 
 #include <iostream>  
@@ -43,10 +43,11 @@ int main()
     return 0;
 }
 
-// Input: 
-// Binary form of 23 is 10111
-// Decimal form of 10101 is 21
+/* Input: 
+   Binary form of 23 is 10111
+   Decimal form of 10101 is 21
 
-// Output: 
-// Binary form of 23 is 10111
-// Decimal form of 10101 is 21
+   Output: 
+   Binary form of 23 is 10111
+   Decimal form of 10101 is 21
+*/
