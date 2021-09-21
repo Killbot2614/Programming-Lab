8c90b1728c2e614789a36fcb90f7d687dd89711a
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-09-21 13:33:02 +0300

    Update Binary Tree Traversal - Iterative Approach 21.09.2021.java

--- Semester 3/Programming in Java/Binary Tree Traversal - Iterative Approach 21.09.2021.java
+++ Semester 3/Programming in Java/Binary Tree Traversal - Iterative Approach 21.09.2021.java
@@ -77,9 +77,9 @@ class Node {
    (1)
    Output: Preorder:20 40 60 70 50 80 90
            Inorder:60 40 70 20 80 50 90
-		   Postorder:60 70 40 80 90 50 20
+           Postorder:60 70 40 80 90 50 20
    (2)
    Output: Preorder:1 2 4 5 3 6 7
            Inorder:4 2 5 1 6 3 7
-		   Postorder:4 5 2 6 7 3 1
+	   Postorder:4 5 2 6 7 3 1
 */
