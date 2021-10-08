7f41b46063643d24bf44443cfd0b285f3ff98ff1
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-10-08 10:49:33 +0300

    Update Exp_03 - Perfect Square Or Not.java

--- Semester 3/Programming in Java/Constructors set 2/Exp_03 - Perfect Square Or Not.java
+++ Semester 3/Programming in Java/Constructors set 2/Exp_03 - Perfect Square Or Not.java
@@ -1,43 +1,22 @@
 // Write a Java program to find Given no is perfect square or not using copy constructor
 
-import java.util.Scanner;
-class prog
-{
-    public int a;
-    public prog(int a)
-    {
-        this.a=a;
-    }
-    public prog(prog p1)
-    {
-        int t=0;
-        for(int i=1;i<p1.a;i++)
-        {
-            int pro=i*i;
-            if(pro==p1.a)
-            {
-                t=1;
-                break;
-                
-            }
-        }
-        if(t==1 && p1.a !=100)
-        {
-            System.out.println("It is a perfect square");
-        }
-        if(p1.a==100)
-        {
-            System.out.println("It is not a perfect square");
-        }
-    }
-    public static void main(String [] args)
-    {
-        Scanner s1=new Scanner(System.in);
-        int a=s1.nextInt();
-        prog p=new prog(a);
-        prog p2=new prog(p);
-    }
-}
+import java.util.Scanner;  
+
+public class Main {   
+    static boolean check(double number) {   
+        double sqrt = Math.sqrt(number);
+        return ((sqrt - Math.floor(sqrt)) == 0);   
+    }   
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);  
+        double number=sc.nextDouble();   
+        
+        if (check(number))   
+        System.out.print("It is a perfect square"); 
+        else  
+        System.out.print("It is not a perfect square");   
+    }   
+}   
 
 /*  (1)
   Input: 9
@@ -45,7 +24,7 @@ class prog
 
   (2)
   Input: 100
-  Output: It is not a perfect square
+  Output: It is a perfect square
 
   (3)
   Input: 49
