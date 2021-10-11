4209418e20178e9762f9e021c5a9e51380a06efb
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-10-11 11:16:45 +0300

    Update Exp_19 - Primary Key _ Foreign Concept - Join 2 Table.sql

--- Semester 3/Database Management Systems/Exp_19 - Primary Key _ Foreign Concept - Join 2 Table.sql
+++ Semester 3/Database Management Systems/Exp_19 - Primary Key _ Foreign Concept - Join 2 Table.sql
@@ -2,7 +2,6 @@ CREATE TABLE Departments (
   DepartmentId INT PRIMARY KEY,
   DepartmentName VARCHAR(15) NOT NULL
 );
-INSERT INTO Departments VALUES(011, "CSE"), (012, "IT"), (013, "ECE"), (014, "EEE");
 
 CREATE TABLE Students (
   StudentId INT PRIMARY KEY,
@@ -11,6 +10,7 @@ CREATE TABLE Students (
   DateOfBirth DATE,
   FOREIGN KEY(DepartmentId) REFERENCES Departments(DepartmentId)
 );
-INSERT INTO Students VALUES(3411237, "Bhargavi", 11, "2002-06-12"), (3412389, "Surendran", 12, "2003-11-23"), (3413066, "Domnic Savio", 13, "2001-01-30");
 
+INSERT INTO Departments VALUES(011, "CSE"), (012, "IT"), (013, "ECE"), (014, "EEE");
+INSERT INTO Students VALUES(3411237, "Bhargavi", 11, "2002-06-12"), (3412389, "Surendran", 12, "2003-11-23"), (3413066, "Domnic Savio", 13, "2001-01-30");
 SELECT DepartmentName, StudentId, StudentName FROM Students JOIN Departments WHERE Students.DepartmentId = Departments.DepartmentId;
