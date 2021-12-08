fd0b210e7588e424d04b661c5cd95fecb0c2e9a7
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-12-08 21:38:12 +0530

    Update Exp_02 - Reverse a Given Number.java

--- Semester 3/Code Optimization and Debugging-I/Java/Exp_02 - Reverse a Given Number.java
+++ Semester 3/Code Optimization and Debugging-I/Java/Exp_02 - Reverse a Given Number.java
@@ -18,9 +18,9 @@ public class Main {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
-	System.out.print("Reverse is : ");
-	reverse(num);
-	}
+    System.out.print("Reverse is : ");
+    reverse(num);
+    }
 }
 
 /*  (1) 
