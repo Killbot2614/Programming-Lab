559854e95508b12f7775e952701549702cb4e2b6
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2021-12-08 21:36:48 +0530

    Update Exp_02 - Reverse a Given Number.java

--- Semester 3/Code Optimization and Debugging-I/Java/Exp_02 - Reverse a Given Number.java
+++ Semester 3/Code Optimization and Debugging-I/Java/Exp_02 - Reverse a Given Number.java
@@ -1,4 +1,37 @@
 // Java Program to reverse a given number using Recursive function
 
-// Refer C/Exp_02
-// LMS compiler required C code
+// Java Program to reverse a given number using Recursive function
+
+import java.util.*;
+
+public class Main {
+	public static void reverse(int num) {
+	    if (num < 10) {
+	        System.out.println(num);
+	        return;
+	} else {
+	    System.out.print(num % 10);
+	    reverse(num / 10);
+	}
+}
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+	System.out.print("Reverse is : ");
+	reverse(num);
+	}
+}
+
+/*  (1) 
+    Input: 4562
+	Output: Reverse is : 2654
+	
+	(2)
+	Input: 1234
+	Output: Reverse is : 4321
+	
+	(3)
+	Input: 5006
+	Output: Reverse is : 6005
+*/
