ce9ccecf35deba4ee749026b4f5c3a9e6b0ac548
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2022-01-22 15:20:02 +0530

    Update Exp_08 -  Quick Sort.py

--- Semester 4/Code Optimization and Debugging-II/Python/Exp_08 -  Quick Sort.py
+++ Semester 4/Code Optimization and Debugging-II/Python/Exp_08 -  Quick Sort.py
@@ -1,3 +1,9 @@
+# 8.Quick Sort in Python
+
+# 1.choose a pivot element
+# 2.divide the array in such a way that elements smaller then pivot come before pivot and elements larger then pivot comes later
+# 3.Recursively call quick sort on each of smaller sub arrays
+
 import sys
 
 def quicksort(x,y,z):
@@ -25,3 +31,11 @@ x = list(map(int,sys.stdin.readline().split()))
 quicksort(x, 0, (len(x) - 1))
 for i in x:
     print(i,end = " ")
+    
+#  (1)
+#  Input: 5 2 6 3 9 4
+#  Output: 2 3 4 5 6 9
+   
+#  (2)
+#  Input: 9 3 1 10 5 14
+#  Output: 1 3 5 9 10 14
