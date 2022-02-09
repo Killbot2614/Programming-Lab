58d5399823577216110de2597e79f8ee4580b31a
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2022-02-09 15:02:55 +0530

    Update Exp_11 - Insertion Sort.py

--- Semester 4/Code Optimization and Debugging-II/Python/Exp_11 - Insertion Sort.py
+++ Semester 4/Code Optimization and Debugging-II/Python/Exp_11 - Insertion Sort.py
@@ -5,13 +5,24 @@
 # Array Elements = {70, 50, 30, 10, 60, 40, 20}
 # Sorted Array = {10, 20, 30, 40, 50, 60, 70}
 
-T1 = input("")
-if T1 == "7":
-    print("10 20 30 40 50 60 70")
+def insertionSort(T1):
+    for i in range(1, len(T1)):
+        temp = T1[i]
+        j = i - 1
+        while (j >= 0 and temp < T1[j]):
+            T1[j + 1] = T1[j]
+            j = j - 1
+        T1[j + 1] = temp
 
-T2 = input("")
-if T2 == "66 44 22 11 33 55":
-    print("11 22 33 44 55 66")
+
+n = int(input())
+
+T1 = input().split()
+T2=T1
+T1 = [int(x) for x in T1]
+insertionSort(T1)
+for i in range(len(T1)):
+   print (T1[i],end=' ')
 
 # (1)
 # Input: 7
