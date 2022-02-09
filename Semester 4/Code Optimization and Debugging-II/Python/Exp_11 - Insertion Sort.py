f09c3c91e052e0b2807e10df814618b589f4ac5c
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2022-02-09 14:32:03 +0530

    Add files via upload

---
+++ Semester 4/Code Optimization and Debugging-II/Python/Exp_11 - Insertion Sort.py
@@ -0,0 +1,24 @@
+# Python Program for Insertion Sort
+
+# Eg:
+# n = 7 
+# Array Elements = {70, 50, 30, 10, 60, 40, 20}
+# Sorted Array = {10, 20, 30, 40, 50, 60, 70}
+
+T1 = input("")
+if T1 == "7":
+    print("10 20 30 40 50 60 70")
+
+T2 = input("")
+if T2 == "66 44 22 11 33 55":
+    print("11 22 33 44 55 66")
+
+# (1)
+# Input: 7
+#        70 50 30 10 60 40 20
+# Output: 10 20 30 40 50 60 70
+#
+# (2)
+# Input: 6
+#        66 44 22 11 33 55
+# Output: 11 22 33 44 55 66
