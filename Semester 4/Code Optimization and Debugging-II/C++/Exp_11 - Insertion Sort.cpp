3366e73640d1ab7d85a66e24892dfed3e4f3621a
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2022-02-15 14:21:17 +0530

    Add files via upload

---
+++ Semester 4/Code Optimization and Debugging-II/C++/Exp_11 - Insertion Sort.cpp
@@ -0,0 +1,51 @@
+// C++ Program for Insertion Sort
+/* Eg:
+n = 7
+Array Elements = {70, 50, 30, 10, 60, 20, 40}
+Sorted Array = {10, 20, 30, 40, 50, 60, 70}
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+void insertionSort(int *array, int size) {
+   int k, j;
+   for(int i = 1; i < size; i++) {
+      k = array[i];
+      j = i;
+      while(j > 0 && array[j - 1] > k) {
+         array[j] = array[j - 1];
+         j--;
+      }
+      array[j] = k;
+   }
+}
+
+void display(int *array, int size) {
+   for(int i = 0; i < size; i++)
+      cout << array[i] << " ";
+   cout << "\n";
+}
+
+int main() {
+   int n;
+   cin >> n;
+   int arr[n];
+   for(int i = 0; i < n; i++) {
+      cin >> arr[i];
+   }
+   insertionSort(arr, n);
+   display(arr, n);
+}
+
+/* (1)
+   Input:7
+         70 50 30 10 60 20 40
+   Output: 10 20 30 40 50 60 70
+   
+   (2)
+   Input: 6
+          66 44 22 11 33 55
+   Output: 11 22 33 44 55 66
+*/
