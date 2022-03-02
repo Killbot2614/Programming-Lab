4cefd365cd3ae09492ebddbdce4ab0f7a0e74af8
Albert Jacob <45996655+Killbot2614@users.noreply.github.com>
2022-03-02 14:27:15 +0530

    Add files via upload

---
+++ Semester 4/Code Optimization and Debugging-II/C/Exp_13 - Inorder Traversal.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int item;
+  char ch;
+  struct node* left;
+  struct node* right;
+};
+
+void inorderTraversal(struct node* root) {
+  if (root == NULL) return;
+  inorderTraversal(root->left);
+  if(root->ch == '\0')
+  printf("%d", root->item);
+  else
+  printf("%d%c", root->item, root->ch);
+  inorderTraversal(root->right);
+}
+
+struct node* createNode(int value, char c) {
+  struct node* newNode = malloc(sizeof(struct node));
+  newNode->item = value;
+  newNode->ch = c;
+  newNode->left = NULL;
+  newNode->right = NULL;
+
+  return newNode;
+}
+
+struct node* insertLeft(struct node* root, int value, char c) {
+  root->left = createNode(value, c);
+  return root->left;
+}
+
+struct node* insertRight(struct node* root, int value, char c) {
+  root->right = createNode(value, c);
+  return root->right;
+}
+
+int main() {
+  struct node* root = createNode(25, ',');
+  insertLeft(root, 15, ',');
+  insertRight(root, 30, '\0');
+  inorderTraversal(root);
+}
